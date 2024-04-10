8b5398d266ff10f166284d7c475a241e2cf565e2
Anas7823 <anaselkhiat78@gmail.com>
2024-04-10 10:38:38 +0200

    bdd exemple

---
+++ medecininfo.sql
@@ -0,0 +1,265 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 10 avr. 2024 à 10:37
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `medecininfo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `identifiant` varchar(50) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL DEFAULT 'admin'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`id`, `identifiant`, `mdp`, `role`) VALUES
+(1, 'anas', '$2a$10$zaWQ/ORghoB/gdvFovdf9.pQRmu4RfZLbpOJUTbVN8sDiWNYf7rqi', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `maladie`
+--
+
+CREATE TABLE `maladie` (
+  `id` int(11) NOT NULL,
+  `id_patient` int(11) NOT NULL,
+  `etat` enum('active','gueri') NOT NULL DEFAULT 'active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `medecins`
+--
+
+CREATE TABLE `medecins` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `specialite` varchar(50) NOT NULL,
+  `identifiant` varchar(50) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL DEFAULT 'medecin'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `medecins`
+--
+
+INSERT INTO `medecins` (`id`, `nom`, `prenom`, `specialite`, `identifiant`, `mdp`, `role`) VALUES
+(2, 'Greg', 'Gui', 'dentiste', 'gregui', '$2a$10$t8.UK2HtGnsd5JXorIWqueakaDC4kzAIJiXTs1/E5H2UJ1ZxdKvoK', 'medecin'),
+(3, 'Junior', 'Gui', 'dentiste', 'Junior', '$2a$10$yKbQccM/mZNlRozuLNYD4e82SwvundN9BO.W0yLHoKNN9r7j1EWKW', 'medecin'),
+(4, 'Albert', 'jores', 'dentiste', 'albert', '$2a$10$dIKf6Rmzlib0Oki226GfYOElUWL/Cu/wURkWlV9QqHUfihrCZmPA6', 'medecin');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `patients`
+--
+
+CREATE TABLE `patients` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `id_medecin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `patients`
+--
+
+INSERT INTO `patients` (`id`, `nom`, `prenom`, `id_medecin`) VALUES
+(2, 'Jean', 'Charles', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rendez_vous`
+--
+
+CREATE TABLE `rendez_vous` (
+  `id` int(11) NOT NULL,
+  `id_medecin` int(11) NOT NULL,
+  `id_patient` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `heure` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `rendez_vous`
+--
+
+INSERT INTO `rendez_vous` (`id`, `id_medecin`, `id_patient`, `date`, `heure`) VALUES
+(1, 2, 2, '2024-11-07', '15:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `traitement`
+--
+
+CREATE TABLE `traitement` (
+  `id` int(11) NOT NULL,
+  `id_patient` int(11) NOT NULL,
+  `id_medecin` int(11) NOT NULL,
+  `maladie` varchar(70) NOT NULL,
+  `medicaments` text NOT NULL,
+  `etat` enum('en cours','fini') NOT NULL DEFAULT 'en cours'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `traitement`
+--
+
+INSERT INTO `traitement` (`id`, `id_patient`, `id_medecin`, `maladie`, `medicaments`, `etat`) VALUES
+(1, 2, 2, 'fièvre', 'doliprane, exastil', 'en cours');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `identifiant` (`identifiant`);
+
+--
+-- Index pour la table `maladie`
+--
+ALTER TABLE `maladie`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_patient` (`id_patient`);
+
+--
+-- Index pour la table `medecins`
+--
+ALTER TABLE `medecins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `identifiant` (`identifiant`);
+
+--
+-- Index pour la table `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_medecin` (`id_medecin`);
+
+--
+-- Index pour la table `rendez_vous`
+--
+ALTER TABLE `rendez_vous`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_medecin` (`id_medecin`),
+  ADD KEY `id_patient` (`id_patient`);
+
+--
+-- Index pour la table `traitement`
+--
+ALTER TABLE `traitement`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_medecin` (`id_medecin`),
+  ADD KEY `id_patient` (`id_patient`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `maladie`
+--
+ALTER TABLE `maladie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `medecins`
+--
+ALTER TABLE `medecins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `rendez_vous`
+--
+ALTER TABLE `rendez_vous`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `traitement`
+--
+ALTER TABLE `traitement`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `maladie`
+--
+ALTER TABLE `maladie`
+  ADD CONSTRAINT `maladie_ibfk_1` FOREIGN KEY (`id_patient`) REFERENCES `patients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `patients`
+--
+ALTER TABLE `patients`
+  ADD CONSTRAINT `patients_ibfk_1` FOREIGN KEY (`id_medecin`) REFERENCES `medecins` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `rendez_vous`
+--
+ALTER TABLE `rendez_vous`
+  ADD CONSTRAINT `rendez_vous_ibfk_1` FOREIGN KEY (`id_medecin`) REFERENCES `medecins` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rendez_vous_ibfk_2` FOREIGN KEY (`id_patient`) REFERENCES `patients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `traitement`
+--
+ALTER TABLE `traitement`
+  ADD CONSTRAINT `traitement_ibfk_1` FOREIGN KEY (`id_medecin`) REFERENCES `medecins` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `traitement_ibfk_2` FOREIGN KEY (`id_patient`) REFERENCES `patients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
